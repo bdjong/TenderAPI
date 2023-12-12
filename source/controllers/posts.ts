e76de6a8d47d3c282ab43abad4750338b0cfcc8f
Bobby de Jong <b.djong@inepro.com>
2023-12-12 21:10:20 +0100

    Add basic GET for all users

--- source/controllers/posts.ts
+++ source/controllers/posts.ts
@@ -34,7 +34,6 @@ const getPosts = async (req: Request, res: Response, next: NextFunction) => {
 
 // getting all events
 const getEvents = async (req: Request, res: Response, next: NextFunction) => {
-  // get some events
   // Example: Get documents from Firestore
   try {
     const collectionRef = firestore.collection("events");
@@ -57,79 +56,32 @@ const getEvents = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-// getting a single post
-const getPost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the post id from the req
-  let id: string = req.params.id;
-  // get the post
-  let result: AxiosResponse = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
-  let post: Post = result.data;
-  return res.status(200).json({
-    message: post,
-  });
-};
+// getting all users
+const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  // Example: Get documents from Firestore
+  try {
+    const collectionRef = firestore.collection("users");
+    const snapshot = await collectionRef.get();
 
-// updating a post
-const updatePost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the post id from the req.params
-  let id: string = req.params.id;
-  // get the data from req.body
-  let title: string = req.body.title ?? null;
-  let body: string = req.body.body ?? null;
-  // update the post
-  let response: AxiosResponse = await axios.put(
-    `https://jsonplaceholder.typicode.com/posts/${id}`,
-    {
-      ...(title && { title }),
-      ...(body && { body }),
+    if (snapshot.empty) {
+      console.log("No documents found.");
+      return;
     }
-  );
-  // return response
-  return res.status(200).json({
-    message: response.data,
-  });
-};
 
-// deleting a post
-const deletePost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the post id from req.params
-  let id: string = req.params.id;
-  // delete the post
-  let response: AxiosResponse = await axios.delete(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
-  // return response
-  return res.status(200).json({
-    message: "post deleted successfully",
-  });
-};
+    snapshot.forEach((doc) => {
+      console.log("Document data:", doc.id, doc.data());
+    });
 
-// adding a post
-const addPost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the data from req.body
-  let title: string = req.body.title;
-  let body: string = req.body.body;
-  // add the post
-  let response: AxiosResponse = await axios.post(
-    `https://jsonplaceholder.typicode.com/posts`,
-    {
-      title,
-      body,
-    }
-  );
-  // return response
-  return res.status(200).json({
-    message: response.data,
-  });
+    return res.status(200).json({
+      message: snapshot,
+    });
+  } catch (error) {
+    console.error("Error getting documents:", error);
+  }
 };
 
 export default {
   getPosts,
-  getPost,
-  updatePost,
-  deletePost,
-  addPost,
   getEvents,
+  getUsers,
 };
--- source/routes/posts.ts
+++ source/routes/posts.ts
@@ -3,12 +3,13 @@ import express from "express";
 import controller from "../controllers/posts";
 const router = express.Router();
 
+//Test function
 router.get("/posts", controller.getPosts);
-router.get("/posts/:id", controller.getPost);
-router.put("/posts/:id", controller.updatePost);
-router.delete("/posts/:id", controller.deletePost);
-router.post("/posts", controller.addPost);
 
+//Events
 router.get("/events", controller.getEvents);
+
+//Users
+router.get("/users", controller.getUsers);
 
 export = router;
