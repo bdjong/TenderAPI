7d0d6c83c3a5afab9486a976a5acf855bebb3fd7
Bobby de Jong <b.djong@inepro.com>
2023-12-12 20:51:30 +0100

    Add basic get events request

--- source/controllers/posts.ts
+++ source/controllers/posts.ts
@@ -1,6 +1,7 @@
 /** source/controllers/posts.ts */
 import { Request, Response, NextFunction } from "express";
 import axios, { AxiosResponse } from "axios";
+import * as admin from "firebase-admin";
 
 interface Post {
   userId: Number;
@@ -8,6 +9,16 @@ interface Post {
   title: String;
   body: String;
 }
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require("../../tender-app-ab614-firebase-adminsdk-fpe2i-a30fc17b3f.json"); // Path to the downloaded JSON file
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tender-app-ab614.firebaseio.com", // Replace with your Firebase project URL
+});
+
+// Get a reference to Firestore
+const firestore = admin.firestore();
 
 // getting all posts
 const getPosts = async (req: Request, res: Response, next: NextFunction) => {
@@ -19,6 +30,31 @@ const getPosts = async (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({
     message: posts,
   });
+};
+
+// getting all events
+const getEvents = async (req: Request, res: Response, next: NextFunction) => {
+  // get some events
+  // Example: Get documents from Firestore
+  try {
+    const collectionRef = firestore.collection("events");
+    const snapshot = await collectionRef.get();
+
+    if (snapshot.empty) {
+      console.log("No documents found.");
+      return;
+    }
+
+    snapshot.forEach((doc) => {
+      console.log("Document data:", doc.id, doc.data());
+    });
+
+    return res.status(200).json({
+      message: snapshot,
+    });
+  } catch (error) {
+    console.error("Error getting documents:", error);
+  }
 };
 
 // getting a single post
@@ -89,4 +125,11 @@ const addPost = async (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
-export default { getPosts, getPost, updatePost, deletePost, addPost };
+export default {
+  getPosts,
+  getPost,
+  updatePost,
+  deletePost,
+  addPost,
+  getEvents,
+};
